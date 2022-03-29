b92fefbacd6357b215574cbf9fbad40d01861740
vickytyen <dsnyen@gmail.com>
2022-03-29 12:26:01 -0300

    Navbar II

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import AppNavbar from './components/Navbar.jsx';
+import AppNavbar from './components/Navbar.js';
 
 function App() {
-  return (
+  return <>
    <AppNavbar/>
-  );
+   <h2>TODOS TUS JUEGOS FAVORITOS!</h2>
+  </>;
 }
 
 export default App;
